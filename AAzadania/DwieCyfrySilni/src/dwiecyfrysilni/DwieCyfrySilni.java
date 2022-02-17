9f65802b38010a91d124e706d91925aeb1e67346
JagodaWiecek <91847547+JagodaWiecek@users.noreply.github.com>
2022-02-17 09:45:52 +0100

    e-lerning

---
+++ AAzadania/DwieCyfrySilni/src/dwiecyfrysilni/DwieCyfrySilni.java
@@ -0,0 +1,55 @@
+
+package dwiecyfrysilni;
+
+public class DwieCyfrySilni {
+
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        int a =3;
+         Foo foo=new Foo();
+         foo.silnia = f.factorial(a);
+         System.out.println(foo.silnia);
+         f.iled(foo); // ile dziesiątek
+         f.ilej(foo); // ile jedynek
+         System.out.println(foo.dziesiec+"  "+foo.jeden);
+    }
+    
+}
+class Factorial
+{
+    int factorial(int n)
+    {
+        int nn=1;
+        for(int i=1;i<=n;i++)
+        {
+            nn*=i;
+        }
+        if(n<2)
+            return nn=1;
+        else if (n>=2)
+            return nn;
+        return 0;
+    }
+    void iled(Foo var)
+    {
+         while (var.silnia>=10)
+        {
+            var.silnia=var.silnia-10;
+            var.dziesiec++;
+        }
+    }
+    void ilej(Foo var)
+    {
+        while (var.silnia>0)
+        {
+            var.silnia--;
+            var.jeden++;
+        }
+    }
+}
+class Foo
+{
+    int silnia=0;
+    int dziesiec=0;
+    int jeden=0;
+}
---
+++ Referencje/src/referencje/Referencje.java
@@ -0,0 +1,46 @@
+
+package referencje;
+
+
+public class Referencje {
+
+    public static void main(String[] args) {
+        
+        int a = 53;
+        int b = a;
+        b = 10;
+        
+        Test x = new Test(); //w x nie znajduje się żadna wartość zwykła typowa prymitywna TYLKO ADRES
+        Test y = x;
+        y.a=40;
+        System.out.println(x.a);
+        
+        a = x.changeVariable(a);
+        System.out.println(a); 
+        
+        Foo foo = new Foo();
+        x.change(foo);
+        System.out.println(foo.y);
+        
+        
+    }
+    
+}
+class Test
+{
+    int a = 20;
+    
+    int changeVariable(int var)
+    {
+        var=var+10;
+        return var;
+    }
+    void change (Foo var)
+    {
+        var.y=var.y+20;
+    }
+}
+class Foo
+{
+    int y = 20;
+}
