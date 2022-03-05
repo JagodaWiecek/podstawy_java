de20fd2feb69a69c28ea9edafa6664e1079c9503
JagodaWiecek <91847547+JagodaWiecek@users.noreply.github.com>
2022-03-05 18:07:14 +0100

    instanceof

---
+++ Main/src/main/Main.java
@@ -0,0 +1,63 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Osoba[] osoba = new Osoba[4];
+        osoba[0]=new Pracownik("Jagoda","Wiecek",1000);
+        osoba[1]= new Student("Jagoda","Więcek");
+        
+        for(Osoba person: osoba)
+        {
+            if(person instanceof Osoba)
+                person.PobierzOpis();
+            else
+                break;
+        }
+    }
+    
+}
+abstract class Osoba
+{
+    String imie;
+    String nazwisko;
+    
+    Osoba(String imie,String nazwisko)
+    {
+        this.imie=imie;
+        this.nazwisko=nazwisko;
+    }
+    abstract void PobierzOpis();
+}
+class Pracownik extends Osoba
+{
+    double wynagrodzenie;
+    Pracownik(String imie,String nazwisko, double wynagrodzenie)
+    {
+        super(imie,nazwisko);
+        this.wynagrodzenie=wynagrodzenie;
+    }
+    void PobierzOpis()
+    {
+        System.out.println("Jestem Pracownikiem");
+        System.out.println("Imię: "+imie);
+        System.out.println("Nazwisko: "+nazwisko);
+        System.out.println("Wynagrodzenie: "+wynagrodzenie+"\n");
+    }
+}
+class Student extends Osoba
+{
+    Student(String imie,String nazwisko)
+    {
+        super(imie,nazwisko);
+    }
+    void PobierzOpis()
+    {
+        System.out.println("Jestem Studentem");
+        System.out.println("Imię: "+imie);
+        System.out.println("Nazwisko: "+nazwisko+"\n");
+    }
+    
+}
